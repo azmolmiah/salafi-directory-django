7f7952b1b94e0dfabd499c9c3f1f4b17ec3e7c41
azmolmiah <azmol@azmolmiah.com>
2020-05-17 15:27:14 +0100

    Model
    Models registered

--- organisations/models.py
+++ organisations/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-
+from django_countries.fields import CountryField
 class Organisation(models.Model):
     name = models.CharField(max_length=100)
     website = models.URLField()
@@ -8,7 +8,7 @@ class Organisation(models.Model):
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=20)
     zipcode = models.CharField(max_length=20)
-    country = models.CharField(max_length=100)
+    country = CountryField()
     description = models.TextField(blank=True)
     types = models.CharField(max_length=20, choices=[('Centre','Centre'), ('School','School'),('Store','Store'),('Pilgrimage','Pilgrimage'),('Charity','Charity')])
     logo = models.ImageField(upload_to='logos/')
