9f249cc4ffc7f10f06f83f16f3457edf545f4049
azmolmiah <azmol@azmolmiah.com>
2020-05-20 13:02:05 +0100

    Classes
    Added logic for GeoIP2 to list latest classes based on user country

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.gis.geoip2 import GeoIP2
 from classes.models import Class
+from organisations.models import Organisation
 
 def index(request):
-    classes = Class.objects.all()[:3]
+    g = GeoIP2('GeoLite2-Country_20200519')
+    country_code = g.country_code('15.194.1.177')
+    organisations = Organisation.objects.filter(country=f'{country_code}')
+    classes = Class.objects.all().filter(organisation__in=organisations).order_by('-created')[:3]
     context = {
         'classes': classes
     }
--- salafi_directory/settings.py
+++ salafi_directory/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'salafi_directory.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
