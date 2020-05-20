32c324723024ccc03d394db5f2a703fa8879320b
azmolmiah <azmol@azmolmiah.com>
2020-05-20 02:41:21 +0100

    classes
    Added latest classes UI to homepage

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.gis.geoip2 import GeoIP2
+from classes.models import Class
 
 def index(request):
-    return render(request, 'pages/index.html')
+    classes = Class.objects.all()[:3]
+    context = {
+        'classes': classes
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
