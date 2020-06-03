f3fc2da5c89292200f392ad550c653a02ed5efdd
azmolmiah <azmol@azmolmiah.com>
2020-06-03 19:17:47 +0100

    Maps
    Added all the map markers using HERE geolocation api

--- maps/views.py
+++ maps/views.py
@@ -1,9 +1,32 @@
+import requests
+import json
 from django.shortcuts import render
+
 from organisations.models import Organisation
 
+
 def index(request):
-  organistion = Organisation.objects.filter(types='Centre')
+  organisations = Organisation.objects.all()
+
+  latlng = []
+
+  for organisation in organisations:
+        URL = "https://geocode.search.hereapi.com/v1/geocode"
+        # Acquire from developer.here.com
+        api_key = 'LNGJKlqzncGWlpgjAy0svm4OGEl0FdN2Cpk_XJrQOQs'
+        PARAMS = {'apikey': api_key, 'q': organisation.address +
+                  ',' + organisation.zipcode}
+
+        # sending get request and saving the response as response object
+        r = requests.get(url=URL, params=PARAMS)
+        data = r.json()
+        latitude = data['items'][0]['position']['lat']
+        longitude = data['items'][0]['position']['lng']
+        lat_lng_coords = float(latitude),float(longitude)
+        latlng.append(lat_lng_coords)
+
   context = {
-    'org': organistion,
+    'latlng': json.dumps(latlng)
   }
-  return render(request, 'maps/maps.html')
+
+  return render(request, 'maps/maps.html', context)
