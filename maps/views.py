19e2eb7bb18c204c0f80d37188c40d71f13083b1
azmolmiah <azmol@azmolmiah.com>
2020-06-04 22:43:23 +0100

    Maps
    Fixed HERE api url params for precise marker locations

--- maps/views.py
+++ maps/views.py
@@ -12,7 +12,7 @@ def index(request):
       URL = "https://geocode.search.hereapi.com/v1/geocode"
       # Acquire from developer.here.com
       api_key = 'LNGJKlqzncGWlpgjAy0svm4OGEl0FdN2Cpk_XJrQOQs'
-      PARAMS = {'apikey': api_key, 'q': organisation.address + ',' + organisation.city + ',' + organisation.zipcode}
+      PARAMS = {'apikey': api_key, 'q': organisation.address + ',' + organisation.city + ',' + organisation.zipcode + ' ' + organisation.country.code}
       # sending get request and saving the response as response object
       r = requests.get(url=URL, params=PARAMS)
       data = r.json()
