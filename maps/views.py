b1626cf1158989c63efb4e3754a63692a5637d0a
azmolmiah <azmol@azmolmiah.com>
2020-06-04 22:12:57 +0100

    Mapsa
    Added cookie session to be used for 5 days instead of requesting API all the time

--- maps/views.py
+++ maps/views.py
@@ -1,35 +1,38 @@
 import requests
 import json
 from django.shortcuts import render
-
 from organisations.models import Organisation
-
 
 def index(request):
   organisations = Organisation.objects.all()
-
   latlng = []
 
-  for organisation in organisations:
-    URL = "https://geocode.search.hereapi.com/v1/geocode"
-    # Acquire from developer.here.com
-    api_key = 'LNGJKlqzncGWlpgjAy0svm4OGEl0FdN2Cpk_XJrQOQs'
-    PARAMS = {'apikey': api_key, 'q': organisation.address + ',' + organisation.city + ',' + organisation.zipcode}
-    # sending get request and saving the response as response object
-    r = requests.get(url=URL, params=PARAMS)
-    data = r.json()
-    latitude = data['items'][0]['position']['lat']
-    longitude = data['items'][0]['position']['lng']
-    details = {
-      "lat": float(latitude),
-      "lng": float(longitude),
-      "name": organisation.name,
-      "address": organisation.address,
-      "city": organisation.city,
-      "zipcode": organisation.zipcode
-    }
-    latlng.append(dict(details))
-  
+  if 'data' not in request.session:
+    for organisation in organisations:
+      URL = "https://geocode.search.hereapi.com/v1/geocode"
+      # Acquire from developer.here.com
+      api_key = 'LNGJKlqzncGWlpgjAy0svm4OGEl0FdN2Cpk_XJrQOQs'
+      PARAMS = {'apikey': api_key, 'q': organisation.address + ',' + organisation.city + ',' + organisation.zipcode}
+      # sending get request and saving the response as response object
+      r = requests.get(url=URL, params=PARAMS)
+      data = r.json()
+      latitude = data['items'][0]['position']['lat']
+      longitude = data['items'][0]['position']['lng']
+      details = {
+        "lat": float(latitude),
+        "lng": float(longitude),
+        "name": organisation.name,
+        "address": organisation.address,
+        "city": organisation.city,
+        "zipcode": organisation.zipcode
+      }
+      latlng.append(dict(details))
+      request.session['data'] = latlng
+      request.session.set_expiry(60 * 60 * 24 * 5)
+      print('New request')
+  else:
+      latlng = request.session['data']
+
   context = {
     'latlng': json.dumps(latlng)
   }
