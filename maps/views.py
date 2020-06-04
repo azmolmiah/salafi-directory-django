95fec261341fd4a80ea077da5f5c9f1f13c04a83
azmolmiah <azmol@azmolmiah.com>
2020-06-04 20:21:47 +0100

    Maps
    Added the maps with marker dictionary details

--- maps/views.py
+++ maps/views.py
@@ -11,20 +11,25 @@ def index(request):
   latlng = []
 
   for organisation in organisations:
-        URL = "https://geocode.search.hereapi.com/v1/geocode"
-        # Acquire from developer.here.com
-        api_key = 'LNGJKlqzncGWlpgjAy0svm4OGEl0FdN2Cpk_XJrQOQs'
-        PARAMS = {'apikey': api_key, 'q': organisation.address +
-                  ',' + organisation.zipcode}
-
-        # sending get request and saving the response as response object
-        r = requests.get(url=URL, params=PARAMS)
-        data = r.json()
-        latitude = data['items'][0]['position']['lat']
-        longitude = data['items'][0]['position']['lng']
-        lat_lng_coords = float(latitude),float(longitude)
-        latlng.append(lat_lng_coords)
-
+    URL = "https://geocode.search.hereapi.com/v1/geocode"
+    # Acquire from developer.here.com
+    api_key = 'LNGJKlqzncGWlpgjAy0svm4OGEl0FdN2Cpk_XJrQOQs'
+    PARAMS = {'apikey': api_key, 'q': organisation.address + ',' + organisation.city + ',' + organisation.zipcode}
+    # sending get request and saving the response as response object
+    r = requests.get(url=URL, params=PARAMS)
+    data = r.json()
+    latitude = data['items'][0]['position']['lat']
+    longitude = data['items'][0]['position']['lng']
+    details = {
+      "lat": float(latitude),
+      "lng": float(longitude),
+      "name": organisation.name,
+      "address": organisation.address,
+      "city": organisation.city,
+      "zipcode": organisation.zipcode
+    }
+    latlng.append(dict(details))
+  
   context = {
     'latlng': json.dumps(latlng)
   }
