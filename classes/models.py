7a54fdaabe44129d1a813272a924f1daa3e61f1b
azmolmiah <azmol@azmolmiah.com>
2020-06-19 14:22:40 +0100

    Maps
    Map Icon codes added to page

--- classes/models.py
+++ classes/models.py
@@ -8,37 +8,50 @@ from django.core.files.images import get_image_dimensions
 
 from organisations.models import Organisation
 from teachers.models import Teacher
+
+
 class Class(models.Model):
-    account = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
-    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE, null=True)
+    account = models.ForeignKey(
+        get_user_model(), null=True, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(
+        Organisation, on_delete=models.CASCADE, null=True)
     teacher = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=28, help_text='Max 30 characters')
-    sub_Heading = models.CharField(max_length=30, blank=True, help_text='Max 30 characters')
-    description = models.TextField(max_length=132, help_text='Must be equal 132 characters.', validators=[MinLengthValidator(132)])
-    date_And_Time = models.DateTimeField(default=timezone.now, help_text='The day and time will be used for every week.')
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=0, help_text='Eg: 0 = free / 1.22 = 1.22')
+    sub_Heading = models.CharField(
+        max_length=40, blank=True, help_text='Max 40 characters')
+    description = models.TextField(
+        max_length=132, help_text='Must be equal 132 characters.', validators=[MinLengthValidator(132)])
+    date_And_Time = models.DateTimeField(
+        default=timezone.now, help_text='The day and time will be used for every week.')
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, default=0, help_text='Eg: 0 = free / 1.22 = 1.22')
     language = models.CharField(max_length=20, default='English')
     is_online = models.BooleanField(default=False)
     link = models.URLField(blank=True, help_text='Link to online class.')
-    photo = models.ImageField(upload_to='classes/', default='classes/', help_text='Must be a height, width of 200px and image size under 20 Kilobytes.')
+    photo = models.ImageField(upload_to='classes/', default='classes/',
+                              help_text='Must be a height, width of 200px and image size under 25 Kilobytes.')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def is_live(self):
-        return  self.date_And_Time.day == datetime.now().day and self.date_And_Time.hour == datetime.now().hour
-    
+        return self.date_And_Time.day == datetime.now().day and self.date_And_Time.hour == datetime.now().hour
+
     def clean(self):
         if(Class.objects.count() > 10 and self.pk is None):
-            raise ValidationError("You are only allowed 10 classes. Please, edit or remove a class.")
+            raise ValidationError(
+                "You are only allowed 10 classes. Please, edit or remove a class.")
 
-        if(self.photo.file.size > 20000):
-             raise ValidationError(f'The image is {self.photo.file.size / 1000} Kilobytes. Must be 20 Kilobytes or less.')
-    
+        if(self.photo.file.size > 25000):
+            raise ValidationError(
+                f'The image is {self.photo.file.size / 1000} Kilobytes. Must be 25 Kilobytes or less.')
+
         w, h = get_image_dimensions(self.photo)
         if w != 200:
-            raise ValidationError("The image is %i pixel wide. Must be 200px." % w)
+            raise ValidationError(
+                "The image is %i pixel wide. Must be 200px." % w)
         if h != 200:
-            raise ValidationError("The image is %i pixel high. Must be 200px." % h)
+            raise ValidationError(
+                "The image is %i pixel high. Must be 200px." % h)
 
     def __str__(self):
         return self.title
--- lectures/models.py
+++ lectures/models.py
@@ -11,20 +11,26 @@ from teachers.models import Teacher
 
 def one_day():
     return datetime.today() + timedelta(days=1)
+
+
 class Lecture(models.Model):
-    account = models.ForeignKey( get_user_model(), null=True, on_delete=models.CASCADE)
+    account = models.ForeignKey(
+        get_user_model(), null=True, on_delete=models.CASCADE)
     organisation = models.ForeignKey(
         Organisation, on_delete=models.CASCADE, null=True)
     teacher = models.ManyToManyField(Teacher)
     title = models.CharField(max_length=28, help_text='Max 30 characters')
-    description = models.TextField(max_length=132, help_text='Must be equal 132 characters.', validators=[MinLengthValidator(132)])
+    description = models.TextField(
+        max_length=132, help_text='Must be equal 132 characters.', validators=[MinLengthValidator(132)])
     date_And_Time = models.DateTimeField(default=datetime.today)
     expiration_Date_And_Time = models.DateTimeField(default=one_day)
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=0, help_text='Eg: 0 = free / 1.22 = 1.22')
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, default=0, help_text='Eg: 0 = free / 1.22 = 1.22')
     language = models.CharField(max_length=20, default='English')
     is_online = models.BooleanField(default=False)
     link = models.URLField(blank=True, help_text='Link to online class.')
-    photo = models.ImageField(upload_to='lectures/', default='lectures/', help_text='Must be a height, width of 200px and image size under 20 Kilobytes.')
+    photo = models.ImageField(upload_to='lectures/', default='lectures/',
+                              help_text='Must be a height, width of 200px and image size under 25 Kilobytes.')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -41,15 +47,18 @@ class Lecture(models.Model):
         if(Lecture.objects.count() > 5 and self.pk is None):
             raise ValidationError(
                 "You are only allowed five lectures. Please, edit or remove a lecture.")
-        
-        if(self.photo.file.size > 20000):
-             raise ValidationError(f'The image is {self.photo.file.size / 1000} Kilobytes. Must be 20 Kilobytes or less.')
-        
+
+        if(self.photo.file.size > 25000):
+            raise ValidationError(
+                f'The image is {self.photo.file.size / 1000} Kilobytes. Must be 25 Kilobytes or less.')
+
         w, h = get_image_dimensions(self.photo)
         if w != 200:
-            raise ValidationError("The image is %i pixel wide. Must be 200px" % w)
+            raise ValidationError(
+                "The image is %i pixel wide. Must be 200px" % w)
         if h != 200:
-            raise ValidationError("The image is %i pixel high. Must be 200px" % h)
+            raise ValidationError(
+                "The image is %i pixel high. Must be 200px" % h)
 
     def __str__(self):
         return self.title
