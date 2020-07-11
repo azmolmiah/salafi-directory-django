9a9ea46ef95dfe6e66346f72f121aba387560255
azmolmiah <azmol@azmolmiah.com>
2020-07-11 12:19:39 +0100

    UI
    Cookies permission persisting once removed

--- salafi_directory/static/js/main.js
+++ salafi_directory/static/js/main.js
@@ -1,11 +1,31 @@
 window.onload = function () { 
+  const policy = document.getElementById('policy');
+
+  if(JSON.parse(localStorage.getItem("btn")) === true){
+    policy.innerHTML = '';
+  } else {
+    policy.innerHTML = `<div class="container">
+    <div class="row pt-3 ">
+      <div class="col-8">
+        <p>Salafi Directory uses cookies and caches to give you the best experience. You give us your consent to do so by clicking 'Accept' or by continuing to use our website after you have received this cookies and caches notification. View our <a href="{% url 'cookies' %}">Cookies and Caches Policy</a> and our <a href="{% url 'privacy' %}">Privacy Policy</a>.</p>
+      </div>
+      <div class="col-4">
+        <button id="policyBtn" class="btn btn-primary btn-block py-auto">
+          Accept
+        </button>
+      </div>
+    </div>
+  </div>`
+  }
+
+  const policyBtn = document.getElementById('policyBtn');
+  policyBtn.addEventListener('click', (e) => {
+    e.path[4].remove();
+    localStorage.setItem("btn", JSON.stringify(true));
+  })
+
   const date = new Date();
   document.querySelector('.year').innerHTML = date.getFullYear();
 
-  const policyBtn = document.getElementById('policyBtn');
-
-  policyBtn.addEventListener('click', (e) => {
-    e.path[4].remove();
-  })
 }
 
